4d66ce1018272e054bfdd9752e2029cd8abb5d9b
Anas <anasjr97@gmail.com>
2023-06-11 23:32:00 +0100

    Pokemon done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Main from './Components/Main';
-import './Components/style.css'
+import React from "react";
+import Main from "./Components/Main";
+import "./Components/style.css";
 function App() {
   return (
     <>
-      <Main/>
+      <Main />
     </>
   );
 }
